023e75ec684c40a6cbd436456b531b2252b15629
Aayush Bhalotia <ashubhalotia@gmail.com>
2021-01-06 21:46:46 +0530

    removed node moudles

--- testback/index.js
+++ testback/index.js
@@ -3,11 +3,9 @@ const app =express();
 
 
 
-// const port = 3000
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+ /*const port = 3000
+ app.get('/', (req, res) => res.send('Hello World!'))
+ app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))*/
 
 const port= 8080;
 app.get("/",(req, res) => {
